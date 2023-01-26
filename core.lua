8fcd78cf841f3e10a1a39a25986105a701a187d1
Tacaly <frederick@tacaly.com>
2023-01-26 03:28:14 +0100

    Create core.lua

---
+++ core.lua
@@ -0,0 +1,16 @@
+if WOW_PROJECT_ID ~= WOW_PROJECT_MAINLINE then return end
+
+local addonName = Linkach1
+
+local Linkach1 do
+
+    Linkach1 = {}
+
+    ---@class CallbackRegistryMixin
+    ---@field public RegisterCallback fun(event: string, callback: fun())
+
+    ---@class ScrollBoxBaseMixin : CallbackRegistryMixin
+    ---@field public GetFrames fun(): Frame[]
+    ---@field public Update fun()
+  
+  end
